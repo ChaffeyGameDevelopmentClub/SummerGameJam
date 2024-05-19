9f489869b32f0da4aed899995c84f3b093aa59e9
Coolshard <elangilito2003@outlook.com>
2024-05-18 20:26:30 -0700

    Spawner pt 1.5
    Working on making the "for" loop work for all values.

--- Godot/HexagonSpawner.gd
+++ Godot/HexagonSpawner.gd
@@ -7,9 +7,20 @@ func _ready():
 
 func build_map (): 
 	var radius = 9
-	for i  in range (6):
-		print("o")
+	for i  in range (1,7):
+		var x = 1*cos(i*45)
+		var z = 1*sin(i*45)
+		print(x,z)
 		var new_hexagon = hexagon.instantiate()
-		new_hexagon.initialize(Vector3.ZERO)
+		new_hexagon.initialize(Vector3(x, 0, z))
 		add_child(new_hexagon)
 		
+	for i in range (1, 13):
+		var x = 2*cos(i*22.5)
+		var z = 2*sin(i*22.5)
+		print(x,z)
+		var new_hexagon = hexagon.instantiate()
+		new_hexagon.initialize(Vector3(x, 0, z))
+		add_child(new_hexagon)
+
+	for 
