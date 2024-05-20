0b9eea3307bf45fedaa2be80b0ff301015f94aa0
Fire1Shark <adrianpeinado2004@gmail.com>
2024-05-19 19:38:45 -0700

    Quickie

--- Godot/script/adrian_global.gd
+++ Godot/script/adrian_global.gd
@@ -2,5 +2,5 @@ extends Node
 
 var coin1 = 0
 var coin2 = 0
-var coin3 = 0
+var coin3 = 20
 var coin4 = 0
--- Godot/script/dead_timer_script.gd
+++ Godot/script/dead_timer_script.gd
@@ -4,22 +4,33 @@ extends Node2D
 #player_ID
 #if player_ID = 1
 	#$CoinPlayer1.start()
-	
-#Checks if players coins are less than 10
-func _process(delta)-> void:
+
+func _ready():
 	$CoinPlayer1.start()
-	if AdrianGlobal.coin1 <= 50:
-		_on_coin_player_1_timeout()
-		print('lol')
+	$CoinPlayer2.start()
+	$CoinPlayer3.start()
+	$CoinPlayer4.start()
+
+#Checks if players coins are less than 10
+func _process(delta):
+	print(AdrianGlobal.coin1)
 
 
 
 func _on_coin_player_1_timeout()-> void:
-	AdrianGlobal.coin1 += 1
-	print(AdrianGlobal.coin1)
-	if AdrianGlobal.coin1 >= 10:
+	if AdrianGlobal.coin1 <= 50:
+		AdrianGlobal.coin1 += 1
+		print(AdrianGlobal.coin1)
+	if AdrianGlobal.coin1 >= 50:
 		$CoinPlayer1.stop()
 
 
+
+
+
 func _on_button_pressed():
-	AdrianGlobal.coin1 -= 10
+	if AdrianGlobal.coin1 >= 10:
+		AdrianGlobal.coin1 -= 10
+		$CoinPlayer1.start()
+	else:
+		print("NO WORK")
