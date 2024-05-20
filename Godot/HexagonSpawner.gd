5d8b38431771bb2720b410e033063e41f58dd794
Coolshard <elangilito2003@outlook.com>
2024-05-19 23:20:08 -0700

    hexagon spawner 2
    Made a dynamic recursive function that spawns hexagons, needs work

--- Godot/HexagonSpawner.gd
+++ Godot/HexagonSpawner.gd
@@ -1,26 +1,40 @@
 extends Node
-
+var theta = 90.0
+var current_row = 0
+var radius = 1
 @export var hexagon: PackedScene 
+var array: Array
 
 func _ready(): 
 	build_map()
+	find_num (7)
+	for i in array.size(): 
+		print (array [i])
 
 func build_map (): 
-	var radius = 9
-	for i  in range (1,7):
-		var x = 1*cos(i*45)
-		var z = 1*sin(i*45)
-		print(x,z)
+	var num = 0
+	for i  in range (1,find_num(7)):
+		if i == array [current_row]:
+			theta /= 2 
+			current_row += 1
+			radius += 2
+			num = 0
+			
+		var x = radius*cos(num*theta)
+		var z = radius*sin(num*theta)
 		var new_hexagon = hexagon.instantiate()
 		new_hexagon.initialize(Vector3(x, 0, z))
 		add_child(new_hexagon)
+		num += 1
+		print (str(theta)+" " +str(current_row)+" " +str(radius)+" " +str(num))
 		
-	for i in range (1, 13):
-		var x = 2*cos(i*22.5)
-		var z = 2*sin(i*22.5)
-		print(x,z)
-		var new_hexagon = hexagon.instantiate()
-		new_hexagon.initialize(Vector3(x, 0, z))
-		add_child(new_hexagon)
+func find_num (row): 
+	if row == 0:
+		print ("1")
+		array.append(1) 
+		return 1
+	else: 
+		var x = (row*6) + find_num (row-1)
+		array.append(x)
+		return x
 
-	for 
