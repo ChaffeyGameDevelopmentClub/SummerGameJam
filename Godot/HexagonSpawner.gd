a75347d3ac172d414b5346c1775d50cfd9d6d275
Coolshard <elangilito2003@outlook.com>
2024-05-21 20:09:40 -0700

    Crumble Function
    crumb

--- Godot/HexagonSpawner.gd
+++ Godot/HexagonSpawner.gd
@@ -6,30 +6,32 @@ var theta = 120.0
 var current_row = 1.0
 var radius = 0
 var array: Array
-var row = 20
-
+var row = 2
+var rng 
 
 func _ready(): 
+	rng = RandomNumberGenerator.new()
 	build_map()
 	find_num (row)
-
+	
 
 func build_map (): 
 	var num = 0.0
 	var y = 0
 	for i  in range (find_num(row)):
-		await get_tree().create_timer(.01).timeout
+		#await get_tree().create_timer(.01).timeout
 		if i == array [current_row - 1]:
 			theta = 360.0 / (6.0 * (current_row))
 			current_row += 1
 			radius += 2
 			num = 0.0
-			y += .25
+			y = rng.randf_range(0,3)
 		var x = radius*cos(num*deg_to_rad(theta))
 		var z = radius*sin(num*deg_to_rad(theta))
 		var new_hexagon = hexagon.instantiate()
 		add_child(new_hexagon)
 		new_hexagon.initialize(Vector3(x, y, z))
+		new_hexagon.crumble()
 		num += 1
 
 
--- Godot/hexagon.gd
+++ Godot/hexagon.gd
@@ -1,4 +1,11 @@
 extends StaticBody3D
-
+var num = 0.01
 func initialize (pos:Vector3):
 	global_position = pos
+func crumble():
+	$FallTimer.start()
+
+func _on_fall_timer_timeout():
+	position.y -= num
+	num += num/10
+	
