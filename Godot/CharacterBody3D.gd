b7cdd698c00e7710e8bdf713809e865eda477121
Thlipsi <samuelk21.student@gmail.com>
2024-05-21 19:35:09 -0700

    qol

--- Godot/CharacterBody3D.gd
+++ Godot/CharacterBody3D.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -15,6 +15,9 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += -gravity/100
+		SPEED += .05
+	else:
+		SPEED = 5
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
--- Godot/HexagonSpawner.gd
+++ Godot/HexagonSpawner.gd
@@ -1,54 +1,38 @@
 extends Node
+
+@export var hexagon: PackedScene 
+
 var theta = 120.0
 var current_row = 1.0
 var radius = 0
-@export var hexagon: PackedScene 
 var array: Array
-var row = 10
+var row = 20
+
 
 func _ready(): 
 	build_map()
 	find_num (row)
-	print(find_num(row))
-	for i in range (10):
-		print(array[i])
-func build_map (): 
-	#var new_hexagon = hexagon.instantiate()
-	#new_hexagon.initialize(Vector3(-5, 0, 5))
-	#add_child(new_hexagon)
 
+
+func build_map (): 
 	var num = 0.0
 	var y = 0
 	for i  in range (find_num(row)):
-		#print(num)
-		
-		
+		await get_tree().create_timer(.01).timeout
 		if i == array [current_row - 1]:
-			print(i)
 			theta = 360.0 / (6.0 * (current_row))
-			#print(array[current_row] -1)
-			#print(360.0 / (6.0 * (current_row)))
 			current_row += 1
 			radius += 2
 			num = 0.0
-			
-			print()
-			print()
-		#y += 1
-		#print(str(radius*cos(num*deg_to_rad(theta)))+ " " + str(radius*sin(num*deg_to_rad(theta))))
-		#print(num)
-		#print(deg_to_rad(theta))
-		#print(cos(num*deg_to_rad(theta)))
+			y += .25
 		var x = radius*cos(num*deg_to_rad(theta))
 		var z = radius*sin(num*deg_to_rad(theta))
-		
 		var new_hexagon = hexagon.instantiate()
-		new_hexagon.initialize(Vector3(x, y, z))
 		add_child(new_hexagon)
+		new_hexagon.initialize(Vector3(x, y, z))
 		num += 1
-		#print (str(theta)+" " +str(current_row)+" " +str(radius)+" " +str(num))
-		
-		
+
+
 func find_num (row) -> float: 
 	if row == 0:
 		array.append(1) 
