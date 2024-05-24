bc97b80c6cbcad42ff290a10386ec74fd9b7a4ca
lizzymak <lizmaks2@gmail.com>
2024-05-23 19:53:31 -0700

    fixed pause menu

--- Godot/Scripts/Lizzy/pauseMenu.gd
+++ Godot/Scripts/Lizzy/pauseMenu.gd
@@ -1,15 +1,32 @@
 extends Control
-
-
+@onready var pause_Menu = $"."
+var paused = false
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pause_Menu.hide()
+
+
+func pauseMenu(): 
+	if paused:
+		pause_Menu.hide()
+		paused = false
+		Engine.time_scale = 1
+	else:
+		pause_Menu.show()
+		paused = true
+		Engine.time_scale = 0
 
 func _on_button_pressed():
 	get_tree().quit()
+
+func _on_mm_button_pressed():
+	Engine.time_scale = 1
+	get_tree().change_scene_to_file("res://Scenes/Start&PauseMenu/start_menu.tscn")
+	
+func _on_resume_button_pressed():
+	pauseMenu()
