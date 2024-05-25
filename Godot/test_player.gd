1844b110aa27b6b545d9cbebbd59222f43493e86
Thlipsi <samuelk21.student@gmail.com>
2024-05-24 21:28:17 -0700

    porogrss

--- Godot/test_player.gd
+++ Godot/test_player.gd
@@ -7,11 +7,7 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _ready():
-	#$Armature/Skeleton3D.physical_bones_start_simulation()
-	#$Armature/Skeleton3D.physical_bones_start_simulation(["Clavicle_L", "Clavicle_R", "Head", "Spine_1"])
 
-	pass
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -20,7 +16,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		print("jump")
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -31,9 +27,5 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		print(global_position)
-	move_and_slide()
 
-func _input(event):
-	if Input.is_action_pressed("testanim"):
-		$Armature/Skeleton3D.physical_bones_start_simulation(["Clavicle_L", "Clavicle_R", "Head", "Spine_1"])
+	move_and_slide()
